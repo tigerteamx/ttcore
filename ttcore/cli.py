6c52ea7e872a20ab9f12847f3ba41e233e90f5ae
Kurhanskakr <kristinaholmovskaa@gmail.com>
2023-06-07 09:20:24 +0300

    updates

--- ttcore/cli.py
+++ ttcore/cli.py
@@ -1,55 +1,86 @@
 #!/usr/bin/env python3
 
+import os
 import shutil
-import json
-
-from docopt import docopt
-
-from .messages import init_message
 
 
-def check_disk(path: str, config: dict = None) -> str:
+def check_disk(path: str, space: float = None, message=None) -> str:
     total, used, free = shutil.disk_usage(path)
     percent_used = (used / total) * 100
+    percent_free = (free / total) * 100
 
-    bytes_in_gb = 1000000000
+    used_mb = used / (1024 ** 2) if used > 0 else 0
+    free_mb = free / (1024 ** 2) if free > 0 else 0
 
-    used_gb = used / bytes_in_gb if used > 0 else 0
-    free_gb = free / bytes_in_gb if free > 0 else 0
+    available_space_msg = ""
 
-    msg = f"The disk at '{path}' has sufficient space:" if percent_used <= 90 else f"The disk at '{path}' is running out of space:"
-    res_msg = f"{msg} ({percent_used:.2f}%)\nLeft: {free_gb:.2f} GB\nUsed: {used_gb:.2f} GB\n"
+    if space is not None:
+        available_space_msg = (
+            f"The disk at '{path}' has {space} MB of free space.\n"
+            if free_mb >= space
+            else f"There is no free {space} MB left at {path}.\n"
+        )
 
-    if not (config and config.get("message", None)):
-        return res_msg
+    stats = f"Left: {free_mb:.2f} ({percent_free:.2f}%) MB\nUsed: {used_mb:.2f} MB ({percent_used:.2f}%)\n"
+    stats += f"'{path}' is running out of space." if percent_used > 90 else f"'{path}' has sufficient space."
 
-    message = init_message(config["message"])
-    message(res_msg)
+    msg = available_space_msg + stats
 
-    return res_msg
+    if message and callable(message):
+        message(msg)
+
+    return msg
 
 
 doc = """ttcore
 
 Usage:
-  ttcore check_disk <path> [--config=<config>]
+  ttcore check_disk <path> [<space>] [--config=<config>]
 
 Options:
   -h --help     Show this screen.
 """
 
 
-def get_config(config):
+def str2float(string):
+    try:
+        return float(string)
+    except:  # noqa
+        raise Exception(f"{string} should be float type.")
+
+
+def read_config(config, mandatory_fields=[]):
+    from json import loads
+
+    if not os.path.isfile(config):
+        raise Exception(f"{config} doesn't exist.")
+
     with open(config, "r") as f:
-        return json.loads(f.read())
+        content = loads(f.read())
+
+    for field in mandatory_fields:
+        if field not in content:
+            raise Exception(f"There is no {field} field found in the {config} file.")
+
+    return content
 
 
 def cli():
+    from docopt import docopt
+
+    from .messages import init_message
+
     arguments = docopt(doc)
 
     if arguments['check_disk']:
-        config = get_config(arguments["--config"]) if arguments["--config"] else None
-        check_disk(arguments['<path>'], config)
+        default_config = os.path.join(os.path.expanduser("~"), ".ttcore.json")
+        free_space = str2float(arguments['<space>']) if arguments['<space>'] else None
+        config_path = arguments["--config"] if arguments["--config"] else default_config
+
+        config = read_config(config_path, ["message"])
+        message = init_message(config["message"])
+
+        check_disk(arguments['<path>'], free_space, message)
 
 
 if __name__ == "__main__":
