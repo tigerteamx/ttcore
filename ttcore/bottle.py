627bf8af3d62b56f14b0a7a7bbb5fd15a59e3528
Kurhanskakr <kristinaholmovskaa@gmail.com>
2023-07-28 19:07:23 +0300

    fix

--- ttcore/bottle.py
+++ ttcore/bottle.py
@@ -142,7 +142,8 @@ def tpost(path, roles=None):
             if roles and not _auth(roles):
                 return HTTPResponse(status=401, body=dict(msg=f"Invalid access. Requires {', '.join(roles)}"))
 
-            data = _get_request_data() if len(params) > 0 else {}
+            req_data = _get_request_data()
+            data = req_data if req_data and len(params) > 0 else {}
             data = _filter_data(data, params)
             _validate_data(data, doc_params)
 
