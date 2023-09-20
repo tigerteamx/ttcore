fcff5d1836ea976e439f547576963668213ed8a0
Kurhanskakr <kristinaholmovskaa@gmail.com>
2023-09-20 15:23:16 +0300

    fix admin

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ttcore",
-    version="0.1.19",
+    version="0.1.20",
     author="Martin F",
     author_email="pypi.org@tigerteamx.com",
     description="Atomic Batteries Included. Used by https://tigerteamx.com to maximize producitivty.",
